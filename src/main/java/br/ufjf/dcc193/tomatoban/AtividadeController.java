42b28c5d539678a9f0f3ed094742fa3d3352483b
Igor Knop <igorknop@gmail.com>
2019-05-29 19:52:52 -0300

    Adciona modelo thymeleaft para index

--- src/main/java/br/ufjf/dcc193/tomatoban/AtividadeController.java
+++ src/main/java/br/ufjf/dcc193/tomatoban/AtividadeController.java
@@ -3,14 +3,17 @@ package br.ufjf.dcc193.tomatoban;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AtividadeController {
     
     @RequestMapping({"","/","/index.html"})
-    @ResponseBody
-    public String index(){
-        return "Hello World!";
+    public ModelAndView index(){
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("atividade-index");
+        mv.addObject("nome", "Fulano");
+        return mv;
     }
     
 }
