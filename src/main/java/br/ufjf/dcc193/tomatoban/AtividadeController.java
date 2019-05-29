074f46fc8201ce9cfa91ff49950c35313fb91a60
Igor Knop <igorknop@gmail.com>
2019-05-29 20:26:44 -0300

    Adiciona create de Atividade

--- src/main/java/br/ufjf/dcc193/tomatoban/AtividadeController.java
+++ src/main/java/br/ufjf/dcc193/tomatoban/AtividadeController.java
@@ -1,20 +1,35 @@
 package br.ufjf.dcc193.tomatoban;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/atividade")
 public class AtividadeController {
-    
-    @RequestMapping({"","/","/index.html"})
-    public ModelAndView atividadeIndex(){
+    @Autowired
+    AtividadeRepository atvRepo;
+
+    @RequestMapping({ "", "/", "/index.html" })
+    public ModelAndView atividadeIndex() {
         ModelAndView mv = new ModelAndView();
         mv.setViewName("atividade-index");
         mv.addObject("nome", "Fulano");
         return mv;
     }
-    
+
+    @RequestMapping("/nova.html")
+    public String nova(){
+
+            return "atividade-form-new.html";
+    }
+
+    @RequestMapping(value="/criar.html", method=RequestMethod.POST)
+    public String criar(Atividade atividade){
+            atvRepo.save(atividade);
+            return "redirect:listar.html";
+    }
+
 }
