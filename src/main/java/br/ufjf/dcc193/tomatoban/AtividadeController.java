81c3265f29ca4c2565b0f6aeea1c5d716d39abf2
Igor Knop <igorknop@gmail.com>
2019-05-29 20:37:03 -0300

    Adciona listagem de atividade

--- src/main/java/br/ufjf/dcc193/tomatoban/AtividadeController.java
+++ src/main/java/br/ufjf/dcc193/tomatoban/AtividadeController.java
@@ -1,7 +1,11 @@
 package br.ufjf.dcc193.tomatoban;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -26,10 +30,18 @@ public class AtividadeController {
             return "atividade-form-new.html";
     }
 
-    @RequestMapping(value="/criar.html", method=RequestMethod.POST)
+    @PostMapping(value="/criar.html")
     public String criar(Atividade atividade){
             atvRepo.save(atividade);
             return "redirect:listar.html";
     }
 
+    @GetMapping(value="/listar.html")
+    public ModelAndView listar(){
+        List<Atividade> atividades = atvRepo.findAll();
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("atividade-list.html");
+        mv.addObject("atividades", atividades);
+        return mv;
+    }
 }
