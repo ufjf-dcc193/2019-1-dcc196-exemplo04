c11f34c8b35e097d249fd10828165ad847a9fcfe
Igor Knop <igorknop@gmail.com>
2019-05-29 20:09:57 -0300

    Adiciona entidade e repositorio

---
+++ src/main/java/br/ufjf/dcc193/tomatoban/Atividade.java
@@ -0,0 +1,54 @@
+package br.ufjf.dcc193.tomatoban;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Atividade {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String titulo;
+    private Integer tomatoes;
+
+    public Atividade(){
+
+    }
+
+    public Atividade(String titulo){
+        setTitulo(titulo);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Integer getTomatoes() {
+        return tomatoes;
+    }
+
+    public void setTomatoes(Integer tomatoes) {
+        this.tomatoes = tomatoes;
+    }
+
+    @Override
+    public String toString() {
+        return "Atividade [id=" + id + ", titulo=" + titulo + ", tomatoes=" + tomatoes + "]";
+    }
+    
+    
+}
